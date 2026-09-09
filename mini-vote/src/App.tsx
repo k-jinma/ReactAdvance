093315882e3b6adf79253bf4e04570d1ef50a809
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2026-09-09 10:44:26 +0900

    CandidateCardコンポーネントに投票数と投票処理関数を追加し、Appコンポーネントで候補者リストを表示する際に使用

--- mini-vote/src/App.tsx
+++ mini-vote/src/App.tsx
@@ -1,5 +1,7 @@
-import type { Candidate } from '../types';
+import type { Candidate } from './types';
 import { CandidateCard } from './components/CandidateCard';
+
+import {useState} from 'react';
 import './App.css';
 
 const CANDIDATES: Candidate[] = [
@@ -9,12 +11,19 @@ const CANDIDATES: Candidate[] = [
 ];
 
 export default function App() {
+  const [votes, setVotes] = useState<Record<string, number>>({});
+  const handleVote = (id: string) => {
+    setVotes( (prev) => ({ ...prev, [id]: (prev[id] ?? 0 ) + 1 }));
+  };
   return(
     <div className="app">
       <h1 className="app__title">ミニ投票</h1>
       <ul className="app_list">
         {CANDIDATES.map((candidate) => (
-          <CandidateCard key={candidate.id} candidate={candidate} />
+          <CandidateCard key={candidate.id} candidate={candidate}
+            count={votes[candidate.id] ?? 0} 
+            onVote={() => handleVote(candidate.id)}
+          />
         ))}
       </ul>
     </div>
--- mini-vote/src/components/CandidateCard.tsx
+++ mini-vote/src/components/CandidateCard.tsx
@@ -2,13 +2,16 @@ import type { Candidate } from '../types';
 
 type Props = {
     candidate: Candidate;
+    count: number;
+    onVote: () => void;
 }
 
-export function CandidateCard({ candidate }: Props){ //Props型からキーcandidateの値を取り出す
+export function CandidateCard({ candidate, count, onVote }: Props){ //Props型からキーcandidateの値を取り出す
     return (
         <li className="card">
             <span className="card__name">{candidate.name}</span>
-            <span className="card__count">0 票</span>
+            <span className="card__count">{count} 票</span>
+            <button type="button" onClick={onVote}>+1票</button>
         </li>
     );
 }
